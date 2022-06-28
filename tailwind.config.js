d591bf76826cd7c8b9a86d22a34c85efc18babe2
Gavin <mypersonal.itp@gmail.com>
2022-06-28 13:34:22 +0700

    Start a project.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
